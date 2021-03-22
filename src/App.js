ca1a37b220ef983b11dd7e1cc432909344741245
Devin Dao-Tse <devindaotse@gmail.com>
2021-03-22 18:23:33 -0400

    added seperate ways for component start(ES6 and regular)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
+import React, {Component, useState} from 'react'
 import './App.css';
 import Person from './Person/Person'
 
+//ES6
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello this is a header tag</h1>
-      <Person name="Devin"/>
-    </div>
-  );
+  switchNameHandler = ()=>{
+        console.log('clicked')
+      }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello this is a header tag</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name="Devin"/>
+        <Person name="Allen"> 
+            some random list
+        </Person>
+      </div>
+    );
+  }
 }
+
+//regular JS
+// function App() {
+//   const switchNameHandler = ()=>{
+//     console.log('clicked')
+//   }
+  
+//   return (
+//     <div className="App">
+//       <h1>Hello this is a header tag</h1>
+//       <button onClick={switchNameHandler}>Switch Name</button>
+//       <Person name="Devin"/>
+//       <Person name="Allen"> 
+//           some random list
+//       </Person>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import './Person.css'
+
 const person = (props) =>{
-    return
-        <div>
+    return (
+        <div className="Person">
             <p>this is {props.name} testing from exporting</p>
+            <p>{props.children}</p>
         </div>
-    
+        )
 }
 
 export default person
